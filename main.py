326e497cad83a9f48d7bba307e671bfa9d5d5f21
juanyepesp <j.yepes@uniandes.edu.co>
2024-05-12 18:34:41 -0500

    feat: add hottest categories endpoint

--- main.py
+++ main.py
@@ -61,8 +61,18 @@ async def get_recommendation_for_user(uid: str):
     return {"spots": for_you_spots, "category": selected_category, "user": uid}
 
 
+@app.get("/hottest_categories")
+async def get_latest_hottest_categories():
+    print('INFO: FETCHING HOTTEST CATEGORIES')
+    hottest_categories = await get_hottest_categories()
+    # Transform the response format
+    transformed_categories = [{"name": category[0], "count": category[1]} for category in hottest_categories]
+    return {"categories": transformed_categories}
+
+
+
 # ----------------------------
-# Trigger updates for aggregated stats
+# Triggers 
 # ----------------------------
 
 @app.get("/trigger_update")
@@ -83,10 +93,6 @@ async def trigger_aggregated_stats_update():
 
     return {}
 
-# ----------------------------
-# Trigger updates for calculating spot categories
-# ----------------------------
-
 @app.get("/trigger_update_categories") # Im leaving this as a get request for testing purposes
 async def trigger_update_categories():
     print('INFO: TRIGGERING UPDATE CATEGORIES')
@@ -98,16 +104,18 @@ async def trigger_update_categories():
     # 2. get all spot reviews
     for spot_id, spot in spots.items():
         spots_reviews[spot_id] = await get_spot_reviews(spot['reviewData']['userReviews'])
-    
+
     # 3. for each spot, read all reviews and sum up the categories
     for spot_id, spot_reviews in spots_reviews.items():
         categories = {}
         for review in spot_reviews:
             for category in review['selectedCategories']:
+                print(category)
                 if category in categories:
                     categories[category] += 1
                 else:
                     categories[category] = 1
+
 
         # 4. update the spot document with the categories
         spot_ref = db.collection('spots').document(spot_id)
@@ -212,3 +220,32 @@ async def restaurants_with_category(selected_category: str):
             if category['name'] == selected_category:
                 spots_with_category.append(spot_id)
     return spots_with_category
+
+
+async def get_hottest_categories():
+    # Query Firestore to get all reviews
+    reviews_ref = db.collection('reviews')
+    reviews = reviews_ref.stream()
+    all_categories = set()  # To store all unique categories
+
+    # Extract categories from each review
+    for review in reviews:
+        review_data = review.to_dict()
+        for category in review_data.get('selectedCategories', []):
+            all_categories.add(category)
+
+    # Count the frequency of each category
+    category_counts = {}
+    for category in all_categories:
+        category_counts[category] = 0
+    
+    reviews = reviews_ref.stream()
+    for review in reviews:
+        review_data = review.to_dict()
+        for category in review_data.get('selectedCategories', []):
+            category_counts[category] += 1
+
+    # Sort categories by count and return the top ones
+    sorted_categories = sorted(category_counts.items(), key=lambda x: x[1], reverse=True)[:5]  # Adjust the number as needed
+
+    return sorted_categories
